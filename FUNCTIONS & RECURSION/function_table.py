534add25cbaacf73e7510598c47d018e74fe8444
usmandesign01-sketch <usmandesign01@gmail.com>
2026-08-23 23:12:15 +0500

    Create function_table.py

---
+++ FUNCTIONS & RECURSION/function_table.py
@@ -0,0 +1,46 @@
+def multi_tab(x):
+    for i in range (1,11):
+        print(x,"*",i,"=",x*i)
+
+multi_tab(2)
+print("\n")
+
+multi_tab(3)
+print("\n")
+
+multi_tab(4)
+print("\n")
+
+multi_tab(5)
+print("\n")
+
+multi_tab(6)
+print("\n")
+
+multi_tab(7)
+print("\n")
+
+multi_tab(8)
+print("\n")
+
+multi_tab(9)
+print("\n")
+
+multi_tab(10)
+print("\n")
+
+multi_tab(11)
+print("\n")
+
+multi_tab(12)
+print("\n")
+
+multi_tab(13)
+print("\n")
+
+multi_tab(14)
+print("\n")
+
+multi_tab(15)
+print("\n")
+
