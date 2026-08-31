e535450079708441c8344ad06779db852df755bb
usmandesign01-sketch <usmandesign01@gmail.com>
2026-08-31 13:07:39 +0500

    Update 05_walkcounting.py

--- recursion/05_walkcounting.py
+++ recursion/05_walkcounting.py
@@ -1,8 +1,15 @@
-def walk(steps):
-    if steps == 10:
-        print("walk time completed")
-        return 
-    print("walk count",steps)
-    walk(steps + 1)
-# print("Your walk time completed")
-walk(1)
+# def walk(steps):
+#     if steps == 10:
+#         print("walk time completed")
+#         return 
+#     print("walk count",steps)
+#     walk(steps + 1)
+# # print("Your walk time completed")
+# walk(1)
+
+def count(number):
+    if number == 0:
+        return 0
+    return number + count(number - 1)
+
+print(count(5))
