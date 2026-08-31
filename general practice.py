3d5df71b69605e9d27a48f976d7b45430a14eaca
usmandesign01-sketch <usmandesign01@gmail.com>
2026-08-31 23:38:00 +0500

    Create general practice.py
    is folder per mera general practice hay

---
+++ general practice.py
@@ -0,0 +1,13 @@
+# my detail
+
+print("Welcome to the world of AI")
+print("My salaray will be 300k $")
+# let's use kwarge function
+def my_det(**person):
+    return person
+
+print(my_det(name = "Usman Khan", age = 19.4, language = "Python",status = "Active learning"))
+
+# how much topic I learn
+
+# I cover most topics like variable and datatype, loops, set , list, dict, tuple, escape sequences, input etc
