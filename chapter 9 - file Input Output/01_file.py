1212bd68a62cd4602695a398263595d0fea3969a
usmandesign01-sketch <usmandesign01@gmail.com>
2026-09-02 21:16:19 +0500

    File Input Output

---
+++ chapter 9 - file Input Output/01_file.py
@@ -0,0 +1,46 @@
+# File I/O hota hi kyun? Use kaha hota hai?
+'''
+Jab program band ho jata hai to RAM ka data delete ho jata jai.
+File I/O se hum data ko hamesha ke liye save kar dete hain.  
+
+'''
+# a = open ("text.txt", "r")
+# x = a.read()
+# print(x)
+# a.close
+
+
+'''
+character       Meaning
+
+'r'     open for reading (default)
+'w'     open for writing, truncating the file first
+'x'     create a new file and open it for writing
+'a'     open for writing, appending to the value of the file it it exists
+'b'     binary mode
+'t'     text mode ( default)
+'+'     open a disk file for updating (reading and wrinting )
+
+'''
+
+# num = input("Enter a number: ")
+# print(num * 2)
+
+# list = ["Python", "C++", "Java", "JavaScript"]
+# print(list)
+# print(list[0])
+# list.append("HTML")
+# print(list)
+# list.remove("Java")
+# print(list)
+# numbers = {1, 2, 2, 3, 3, 4}
+# print(numbers)
+
+# dict = {"name": "Usman Khan",
+#         "age" : 20,
+#         "city": "Karachi"}
+# print(dict.values())
+# print(dict["city"])
+
+a = "D:\communication skill practice\1 35.mp4"
+print(a)
---
+++ chapter 9 - file Input Output/file.py
@@ -0,0 +1,1 @@
+# by code with harry
