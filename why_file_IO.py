f9a91b1a71fa425852606d2bc3ccc64813eff4fb
Usman Khan <usmandesign01@gmail.com>
2026-09-05 23:27:35 +0500

    Add files via upload

---
+++ why_file_IO.py
@@ -0,0 +1,5 @@
+# what will be the benefit of file input output
+'''I can create a file from desktop and here also....confuse'''
+
+with open("general.txt","w") as f:
+    f.write("Why we use file input output ...")
