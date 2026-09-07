77b8167b62c913ba9585a9f803538d0ed9959d04
usmandesign01-sketch <usmandesign01@gmail.com>
2026-09-07 17:02:29 +0500

    Create 01_class.py

---
+++ OOPs/01_class.py
@@ -0,0 +1,11 @@
+print ("Hello I am learning OOPs topic in 07_Sept_2026")
+
+# Class VS Objects
+
+class Car: # Class
+    color = "Blue"
+    model = "Toyota"
+
+car1 = Car # Object
+print(car1.color)
+print(car1.model)
