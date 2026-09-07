f4b1e8d15f5401d9e34913f57fb8a79a1c877dc0
usmandesign01-sketch <usmandesign01@gmail.com>
2026-09-07 23:31:20 +0500

    Update 01_class.py
    Class and Objects

--- OOPs/01_class.py
+++ OOPs/01_class.py
@@ -1,11 +1,30 @@
 print ("Hello I am learning OOPs topic in 07_Sept_2026")
 
-# Class VS Objects
 
-class Car: # Class
+'''OOP in Python :
+    To map with real world scenarios, we started using objects in code.
+    this is called oop.
+    
+    Why we Use?
+    To increase the reusability of program '''
+
+# Class VS Objects
+'''Obj:
+        In real world everything is object like Mouse, Car, Pen, Keyboard etc
+        From everything we can make an objects but first we will make a class
+        list string are object '''
+
+'''Class:
+        Class is a blueprint for creating an objects.
+        Example: let's suppose I have a car we desing a blueprint that every Car will have blue color
+        and 4 seats with AC'''
+
+# How to define a class for what to store
+class Car:              # Class
     color = "Blue"
     model = "Toyota"
 
-car1 = Car # Object
+# How to make an objects
+car1 = Car              # Object
 print(car1.color)
 print(car1.model)
