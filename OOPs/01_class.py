3b0c586212e08a4a5cae348d027f314d832febc6
usmandesign01-sketch <usmandesign01@gmail.com>
2026-09-07 23:51:10 +0500

    Update 01_class.py

--- OOPs/01_class.py
+++ OOPs/01_class.py
@@ -1,4 +1,4 @@
-print ("Hello I am learning OOPs topic in 07_Sept_2026")
+# print ("Hello I am learning OOPs topic in 07_Sept_2026")
 
 
 '''OOP in Python :
@@ -19,12 +19,33 @@ print ("Hello I am learning OOPs topic in 07_Sept_2026")
         Example: let's suppose I have a car we desing a blueprint that every Car will have blue color
         and 4 seats with AC'''
 
-# How to define a class for what to store
-class Car:              # Class
-    color = "Blue"
-    model = "Toyota"
+# Without OOP, you might write:
+dog_name = "Buddy"
+dog_age = 3
 
-# How to make an objects
-car1 = Car              # Object
-print(car1.color)
-print(car1.model)
+def bark(name):
+    return f"{name} says woof!"
+bark(dog_name)
+
+# With OOP, you bundle data + behavior together:
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        return f"{self.name} says woof!"
+
+
+
+
+
+# How to define a class for what to store
+# class Car:              # Class
+#     color = "Blue"
+#     model = "Toyota"
+
+# # How to make an objects
+# car1 = Car              # Object
+# print(car1.color)
+# print(car1.model)
