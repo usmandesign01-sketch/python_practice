62aa0d843c5803c978c79c7b0d643b82b7460313
usmandesign01-sketch <usmandesign01@gmail.com>
2026-09-11 22:52:07 +0500

    Create prob_1.py

---
+++ list & tuple practice set/again practice. list and tuple/prob_1.py
@@ -0,0 +1,16 @@
+'''Problem 1 — Basic List
+
+Create a list containing 5 of your favorite foods.
+
+Print:
+
+the complete list
+the first item
+the last item
+the length of the list'''
+
+fvt_foods = ["Fast Foods", "Fry Beef", "Boiled Potatoes", "Fast Foods", "Dry Fruits"]
+print(f"My fvt foods {fvt_foods}")
+print("The first item is", fvt_foods[0])
+print("The last item is",fvt_foods[4])
+print(f"The length of the list is {len(fvt_foods)}")
