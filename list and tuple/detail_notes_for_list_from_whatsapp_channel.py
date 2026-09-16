17d7d3b655a2a5760f38e872ba84142129c18779
usmandesign01-sketch <usmandesign01@gmail.com>
2026-09-16 21:35:35 +0500

    Update detail_notes_for_list_from_whatsapp_channel.py

--- list and tuple/detail_notes_for_list_from_whatsapp_channel.py
+++ list and tuple/detail_notes_for_list_from_whatsapp_channel.py
@@ -265,9 +265,55 @@ print(my_friends[2])  # Output: Zubair
 my_friends[3]
 print(my_friends[3])  # Output: Usman Khan
 
-# Negative Indexing
+# Python also supports Negative Indexing
 my_friends = ["Abdullah", "Zohaib", "Zubair", "Usman Khan"]
 print(my_friends[-1])  # Output: Usman Khan
 print(my_friends[-2])  # Output: Zubair
 print(my_friends[-3])  # Output: Zohaib
 print(my_friends[-4])  # Output: Abdullah
+
+# Slicing Lists
+my_friends = ["Abdullah", "Zohaib", "Zubair", "Usman Khan"]
+# Slicing from index 1 to 3 (excluding index 3)
+print(my_friends[1:3])  # Output: ['Zohaib', 'Zubair']
+# Slicing from index 0 to 2 (excluding index 2)
+print(my_friends[0:2])  # Output: ['Abdullah', 'Zohaib']
+print(my_friends[2:4])   # Output: ['Zubair', 'Usman Khan']
+print(my_friends[:2])   # Output: ['Abdullah', 'Zohaib']
+print(my_friends[2:4])   # Output: ['Zubair', 'Usman Khan']
+print(my_friends[:4])    # Output: ['Abdullah', 'Zohaib', 'Zubair', 'Usman Khan']
+print(my_friends[:])     # Output: ['Abdullah', 'Zohaib', 'Zubair', 'Usman Khan']
+
+# Lists are Mutable
+my_friends = ["Abdullah", "Zohaib", "Zubair", "Usman Khan"]
+# Changing the second element (index 1) to "Ali"
+my_friends[1] = "Ali"
+print(my_friends)  # Output: ['Abdullah', 'Ali', 'Zubair', 'Usman Khan']
+
+# Adding Elements to a List
+my_friends = ["Abdullah", "Zohaib", "Zubair", "Usman Khan"]
+my_friends.append("Ali")  # Adding "Ali" to the end of the list
+print(my_friends)  # Output: ['Abdullah', 'Zohaib', 'Zubair', 'Usman Khan', 'Ali']
+my_friends.insert(1, "Ahmed")  # Inserting "Ahmed" at index 1
+print(my_friends)  # Output: ['Abdullah', 'Ahmed', 'Zohaib', 'Zubair', 'Usman Khan', 'Ali']
+
+my_friends.insert(3, "Hassan")  # Inserting "Hassan" at index 3
+print(my_friends)  # Output: ['Abdullah', 'Ahmed', 'Zohaib', 'Hassan', 'Zubair', 'Usman Khan', 'Ali']
+my_friends.remove("Zubair")  # Removing "Zubair" from the list
+print(my_friends)  # Output: ['Abdullah', 'Ahmed', 'Zohaib', 'Hassan', 'Usman Khan', 'Ali']
+
+# Finding the Length of a List
+my_friends = ["Abdullah", "Zohaib", "Zubair", "Usman Khan"]
+print(len(my_friends))  # Output: 4
+
+# Checking if an Element Exists in a List
+my_friends = ["Abdullah", "Zohaib", "Zubair", "Usman Khan"]
+print("Usman Kaka Khail" in my_friends)  # Output: False
+print("Usman Khan" in my_friends)  # Output: True
+
+# Looping Through a List
+my_friends = ["Abdullah", "Zohaib", "Zubair", "Usman Khan"]
+for friend in my_friends:
+    print(friend)
+
+    
