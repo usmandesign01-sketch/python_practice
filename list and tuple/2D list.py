fc5929b33945b41ef080c4cde80aafbe385e2c7c
usmandesign01-sketch <usmandesign01@gmail.com>
2026-09-23 16:12:04 +0500

    Create 2D list.py

---
+++ list and tuple/2D list.py
@@ -0,0 +1,17 @@
+fruits = ["apple", "Banana", "Grapes", "Orange" ]
+vegetables = ["spinach", "lady finger", "Tomato", "Potato" ]
+meets = ["Chicken", "Beef", "Mutton", "Fish", "Duck", "Turkey"]
+
+max_things = [fruits, vegetables, meets]
+print(max_things[0][0])
+print(max_things[0][1])
+print(max_things[0][2])
+print()
+print(max_things[1][0])
+print(max_things[1][1])
+print(max_things[1][2])
+print()
+print(max_things[2][0])
+print(max_things[2][1])
+print(max_things[2][2])
+print()
