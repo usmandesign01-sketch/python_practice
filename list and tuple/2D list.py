5306bd6805a96e2855718dc2af174646b6254b46
usmandesign01-sketch <usmandesign01@gmail.com>
2026-09-23 16:14:21 +0500

    Update 2D list.py

--- list and tuple/2D list.py
+++ list and tuple/2D list.py
@@ -1,6 +1,6 @@
-fruits = ["apple", "Banana", "Grapes", "Orange" ]
-vegetables = ["spinach", "lady finger", "Tomato", "Potato" ]
-meets = ["Chicken", "Beef", "Mutton", "Fish", "Duck", "Turkey"]
+fruits =        ["apple", "Banana", "Grapes", "Orange" ]
+vegetables =    ["spinach", "lady finger", "Tomato", "Potato" ]
+meets =         ["Chicken", "Beef", "Mutton", "Fish", "Duck", "Turkey"]
 
 max_things = [fruits, vegetables, meets]
 print(max_things[0][0])
